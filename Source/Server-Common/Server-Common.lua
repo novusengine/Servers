a0f3ef3accbfe75841cabce4492e37a5f0150bd3
Foereaper <foereaper@elunatech.com>
2024-08-01 00:00:44 +0200

    Make server-common depend on lua-analysis

--- Source/Server-Common/Server-Common.lua
+++ Source/Server-Common/Server-Common.lua
@@ -1,4 +1,4 @@
-local mod = Solution.Util.CreateModuleTable("Server-Common", { "base", "fileformat", "input", "network", "gameplay", "luau-compiler", "luau-vm", "enkits", "refl-cpp", "utfcpp", "base64", "libpqxx" })
+local mod = Solution.Util.CreateModuleTable("Server-Common", { "base", "fileformat", "input", "network", "gameplay", "luau-compiler", "luau-vm", "luau-analysis", "enkits", "refl-cpp", "utfcpp", "base64", "libpqxx" })
 
 Solution.Util.CreateStaticLib(mod.Name, Solution.Projects.Current.BinDir, mod.Dependencies, function()
     local defines = { "_CRT_SECURE_NO_WARNINGS", "_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS", "_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS" }
