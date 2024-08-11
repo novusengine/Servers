bf4c605d7ecfef441fd3080216867a75c17ae367
NixAJ <nickiarpejensen@gmail.com>
2024-08-11 17:08:17 +0200

    Updated Submodule Engine

--- Source/Server-Common/Server-Common.lua
+++ Source/Server-Common/Server-Common.lua
@@ -1,4 +1,4 @@
-local mod = Solution.Util.CreateModuleTable("Server-Common", { "base", "fileformat", "input", "network", "gameplay", "luau-compiler", "luau-vm", "luau-analysis", "enkits", "refl-cpp", "utfcpp", "base64", "libpqxx" })
+local mod = Solution.Util.CreateModuleTable("Server-Common", { "base", "fileformat", "input", "network", "gameplay", "luau-compiler", "luau-vm", "enkits", "refl-cpp", "utfcpp", "base64", "libpqxx" })
 
 Solution.Util.CreateStaticLib(mod.Name, Solution.Projects.Current.BinDir, mod.Dependencies, function()
     local defines = { "_CRT_SECURE_NO_WARNINGS", "_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS", "_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS" }
@@ -14,8 +14,6 @@ Solution.Util.CreateStaticLib(mod.Name, Solution.Projects.Current.BinDir, mod.De
     Solution.Util.SetFilter("platforms:Win64", function()
         Solution.Util.SetDefines({"WIN32_LEAN_AND_MEAN", "NOMINMAX"})
     end)
-    
-    dependson {"Luau-Compiler", "Luau-VM"}
 end)
 
 Solution.Util.CreateDep(mod.NameLow, mod.Dependencies, function()
