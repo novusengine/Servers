60b2f122970062e85af296024db25ff12fe9797c
Foe <Foereaper@users.noreply.github.com>
2024-08-11 17:32:14 +0200

    Add Linux build workflow and fix Linux compatibility (#3)
    * Update Engine submodule and add Linux workflow files
    * Add SetLibDirs to premake util
    * Fix upper/lowercase issue in dependencies
    * Define postgres library and include for libpq
    
    ---------
    
    Co-authored-by: NixAJ <nickiarpejensen@gmail.com>

--- Dependencies/libpqxx/Libpqxx.lua
+++ Dependencies/libpqxx/Libpqxx.lua
@@ -1,8 +1,12 @@
-local osEnvName = "POSTGRES_ROOT"
-local postgresRoot = os.getenv(osEnvName)
+local postgresRoot = ""
+-- TODO: should probably not hardcode postgres dir for Linux but oh well
+if os.target() == "windows" then
+    local osEnvName = "POSTGRES_ROOT"
+    postgresRoot = os.getenv(osEnvName)
 
-if not postgresRoot then
-    Solution.Util.PrintError("Failed to find System Environment Variable '" .. osEnvName .. ". Please ensure Postgres is installed and that " .. osEnvName .. " have been properly configured")
+    if not postgresRoot then
+        Solution.Util.PrintError("Failed to find System Environment Variable '" .. osEnvName .. ". Please ensure Postgres is installed and that " .. osEnvName .. " have been properly configured")
+    end
 end
 
 local dep = Solution.Util.CreateDepTable("Libpqxx", {})
@@ -15,7 +19,13 @@ Solution.Util.CreateStaticLib(dep.Name, Solution.Projects.Current.BinDir, dep.De
 
     local baseDir = dep.Path .. "/Libpqxx"
     local sourceDir = baseDir .. "/src"
-    local includeDirs = { baseDir .. "/include", postgresRoot .. "/include" }
+    local pqinclude = iif(os.istarget("windows"), postgresRoot .. "/include", { "/usr/include/postgresql", "/usr/include/postgresql/14/server" })
+    
+    if os.target() == "linux" then
+        Solution.Util.SetLibDirs({ "/usr/lib/x86_64-linux-gnu", "/usr/lib/postgresql/14/lib" })
+    end
+    
+    local includeDirs = { baseDir .. "/include", pqinclude }
     local files =
     {
         sourceDir .. "/**.cxx",
@@ -24,12 +34,15 @@ Solution.Util.CreateStaticLib(dep.Name, Solution.Projects.Current.BinDir, dep.De
     Solution.Util.SetFiles(files)
     Solution.Util.SetIncludes(includeDirs)
     Solution.Util.SetDefines(defines)
-    Solution.Util.SetLinks({postgresRoot .. "/lib/libpq.lib"})
+    
+    local libPath = iif(os.istarget("windows"), postgresRoot .. "/lib/libpq.lib", "pq")
+    Solution.Util.SetLinks(libPath)
 end)
 
 Solution.Util.CreateDep(dep.NameLow, dep.Dependencies, function()
     local baseDir = dep.Path .. "/Libpqxx"
 
     Solution.Util.SetIncludes({baseDir .. "/include"})
-    Solution.Util.SetLinks(dep.Name)
+    local libPath = iif(os.istarget("windows"), postgresRoot .. "/lib/libpq.lib", "pq")
+    Solution.Util.SetLinks({ dep.Name, libPath })
 end)
